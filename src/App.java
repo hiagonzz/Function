46157ceeadd9e60b16ecdd3fc95f99600c506731
Hiago nunes pimentel <derundaclash665@gmail.com>
2023-12-31 00:00:01 -0300

    Expressão lambda inline

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -15,9 +14,8 @@ public class App {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
-        Function<Product, String> func = p -> p .getName().toUpperCase();
 
-        List<String> names = list.stream().map(func).collect(Collectors.toList());
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
         names.forEach(System.out::println);
     }
 }
